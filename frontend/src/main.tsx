9885f2d2fcdf89af8e5c44258605a706a5a29b65
filols <filip.olsson@kits.se>
2024-01-31 20:35:49 +0100

    Refactor base code to CSS-in-JS with MUI/Emotion
    - Transitioned index and App components to MUI/Emotion CSS-in-JS,
      moving away from external .css files.
    - Removed Vite's default css to eliminate conflicting global styles,
      aligning with our component-based styling approach.
    - Introduced CSSBaseline from MUI to normalize styles across browsers,
      ensuring a consistent baseline.
    - Applied viewport-based sizing on the root element for consistency.
      Height-value will probably need modification later.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { CssBaseline } from '@mui/material';
+import { Global, css } from '@emotion/react';
+
+/* eslint-disable react-refresh/only-export-components */
+const GlobalStyles = () => (
+  <Global
+    styles={css`
+      #root {
+        height: 100vh;
+        width: 100%;
+      }
+    `}
+  />
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <CssBaseline />
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,91 @@
 import { signinRedirect } from '@/utils/Auth';
+import Box from '@mui/material/Box';
+// import { styled } from '@mui/material';
+// import Button from '@mui/material/Button';
 
-const login = () => {
+// const TestDiv = styled('div')(({ theme }) => ({
+//   color: theme.palette.primary.main,
+// }));
+
+// const LoginButton = styled(Button)(({ width }: { width: string }) => ({
+//   width: width,
+//   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+// }));
+
+const Wrapper = ({ children }: any) => (
+  <Box
+    sx={{
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      // flexDirection: 'column',
+      // justifyContent: 'flex-start',
+      // alignItems: 'center',
+      // backgroundColor: 'black',
+    }}
+  >
+    {children}
+  </Box>
+);
+
+const Login = () => {
   return (
-    <div>
+    <Wrapper>
       <button onClick={() => signinRedirect()}>Sign In</button>
-    </div>
+    </Wrapper>
   );
 };
 
-export default login;
+// const Login = () => {
+//   return (
+//     <Box sx={
+//       .wrapper {
+//         height: 100%;
+//         width: 100%;
+//         position: relative;
+//         display: flex;
+//         flex-direction: column;
+//         justify-content: flex-start;
+//         align-items: center;
+//       }
+
+//       .header {
+//         width: 100%;
+//         height: 3rem;
+//         background-color: var(--primary-color);
+//         color: #f2f2f2;
+//         text-align: center;
+//       }
+
+//       .login {
+//         width: 29rem;
+//         height: 25rem;
+//         display: flex;
+//         flex-direction: column;
+//         align-items: center;
+//         justify-content: center;
+//         border-radius: 0.3125rem;
+//         background: var(--darkmode-lighter-bg);
+//         box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.25);
+//         position: absolute;
+//         top: 25%;
+//         gap: 2rem;
+//       }
+
+//       .kits {
+//         width: 13rem;
+//         height: 9rem;
+//       }
+
+//     }>
+//       <button onClick={() => signinRedirect()}>Sign In</button>
+//       {/* <LoginButton width={'1000px'} onClick={() => signinRedirect()}>
+//         Sign In
+//       </LoginButton> */}
+//     </Box>
+//   );
+// };
+
+export default Login;
