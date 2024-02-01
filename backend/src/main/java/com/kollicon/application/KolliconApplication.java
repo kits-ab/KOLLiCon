1c1b8bc269a4d6427370460aa57de8f075f65bfe
JohanBengtsson89 <johanbengtsson89@gmail.com>
2024-02-01 15:29:43 +0100

    Added basic CRUD operations for schedule

--- backend/src/main/java/com/kollicon/application/KolliconApplication.java
+++ backend/src/main/java/com/kollicon/application/KolliconApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.kollicon.model")
-@EnableJpaRepositories
+@EnableJpaRepositories("com.kollicon.repository")
 @SpringBootApplication(scanBasePackages = "com.kollicon")
 public class KolliconApplication {
 
