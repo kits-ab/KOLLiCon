aa3f41b8088a4c9dde13e8e73c5ceed53441d459
AlirezaHK91 <kasra_tehran101@hotmail.com>
2024-02-05 19:24:37 +0100

    Fixed the update function so that it can update even nested objects like presenters and location.

--- backend/src/main/java/com/kollicon/service/ActivityService.java
+++ backend/src/main/java/com/kollicon/service/ActivityService.java
@@ -1,8 +1,11 @@
 package com.kollicon.service;
 
 import com.kollicon.model.ActivityModel;
+import com.kollicon.model.LocationModel;
 import com.kollicon.model.PresenterModel;
 import com.kollicon.repository.ActivityRepository;
+import com.kollicon.repository.PresenterRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ import java.util.Optional;
 public class ActivityService {
     @Autowired
      ActivityRepository activityRepository;
+    @Autowired
+    PresenterRepository presenterRepository;
 
 
     public ActivityModel createActivity(ActivityModel activity) {
@@ -32,8 +37,56 @@ public class ActivityService {
         return activityRepository.findById(id).get();
     }
 
-    public ActivityModel updateActivity (ActivityModel activity) {
-        return activityRepository.save(activity);
+    private PresenterModel getPresenterById(Long id) {
+        return presenterRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Presenter not found with id: " + id));
+    }
+
+    public ActivityModel updateActivity(ActivityModel updatedActivity) {
+        ActivityModel existingActivity = getActivityById(updatedActivity.getId());
+
+        // Uppdatera fälten om inte nulla
+        existingActivity.setTitle(updatedActivity.getTitle());
+        existingActivity.setType(updatedActivity.getType());
+        existingActivity.setDetails(updatedActivity.getDetails());
+        existingActivity.setStart(updatedActivity.getStart());
+        existingActivity.setEnd(updatedActivity.getEnd());
+        existingActivity.setWinner(updatedActivity.getWinner());
+
+        // uppdatera presenters
+        updatePresenters(existingActivity, updatedActivity.getPresenter());
+
+        // uppdatera location
+        updateLocation(existingActivity, updatedActivity.getLocation());
+
+        return activityRepository.save(existingActivity);
+    }
+
+    private void updatePresenters(ActivityModel existingActivity, List<PresenterModel> updatedPresenters) {
+        if (updatedPresenters != null) {
+            updatedPresenters.forEach(updatedPresenter -> {
+                PresenterModel existingPresenter = getPresenterById(updatedPresenter.getId());
+
+                // uppdatera fälten om inte nulla
+                existingPresenter.setName(updatedPresenter.getName());
+                existingPresenter.setImage(updatedPresenter.getImage());
+
+                existingPresenter.setActivity(existingActivity);
+                presenterRepository.save(existingPresenter);
+            });
+        }
+    }
+
+    private void updateLocation(ActivityModel existingActivity, LocationModel updatedLocation) {
+        if (updatedLocation != null) {
+            LocationModel existingLocation = existingActivity.getLocation();
+
+            // Update location fields if not null
+            existingLocation.setCoordinates(updatedLocation.getCoordinates());
+            existingLocation.setTitle(updatedLocation.getTitle());
+
+            existingLocation.setActivity(existingActivity);
+        }
     }
 
     public String deleteActivity (Long id) {
