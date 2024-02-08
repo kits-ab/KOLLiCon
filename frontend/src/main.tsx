5fee50fbed27d0d8f6527276114c49c1950fd76d
JohanBengtsson89 <johanbengtsson89@gmail.com>
2024-02-08 19:29:24 +0100

    localised the error to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,32 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import { CssBaseline } from '@mui/material';
-import { Global, css } from '@emotion/react';
-import { theme } from '@/theme/theme.ts';
-import { ThemeProvider } from '@mui/material';
+// import { CssBaseline } from '@mui/material';
+// import { Global, css } from '@emotion/react';
+// import { theme } from '@/theme/theme.ts';
+// import { ThemeProvider } from '@mui/material';
 
 /* eslint-disable react-refresh/only-export-components */
-const GlobalStyles = () => (
-  <Global
-    styles={css`
-      #root {
-        height: 100vh;
-        height: 100svh;
-        width: 100%;
-      },
-    `}
-  />
-);
+// const GlobalStyles = () => (
+//   <Global
+//     styles={css`
+//       #root {
+//         height: 100vh;
+//         height: 100svh;
+//         width: 100%;
+//       },
+//     `}
+//   />
+// );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
