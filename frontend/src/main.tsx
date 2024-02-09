38b6bf69f3a9701883080129cd9f9cd27e051d4f
AlirezaHK91 <113583919+AlirezaHK91@users.noreply.github.com>
2024-02-09 11:01:17 +0100

    Fixed the bugg in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,32 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import { CssBaseline } from '@mui/material';
-import { Global, css } from '@emotion/react';
-import { theme } from '@/theme/theme.ts';
-import { ThemeProvider } from '@mui/material';
-
-/* eslint-disable react-refresh/only-export-components */
-const GlobalStyles = () => (
-  <Global
-    styles={css`
-      #root {
-        height: 100vh;
-        height: 100svh;
-        width: 100%;
-      },
-    `}
-  />
-);
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
