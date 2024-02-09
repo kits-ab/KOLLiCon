0c3142e51f4a78421929a3f3b0aaf7c0e121b515
JohanBengtsson89 <johanbengtsson89@gmail.com>
2024-02-09 15:47:10 +0100

    Import styles from styledActivity

--- frontend/src/components/CreateScheduleComponent.tsx
+++ frontend/src/components/CreateScheduleComponent.tsx
@@ -1,8 +1,13 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
-import { Button, GlobalStyles, WrapperProps, colors } from '@kokitotsos/react-components';
-
-import styled from '@emotion/styled';
+import { GlobalStyles } from '@kokitotsos/react-components';
+import {
+  EventsWrapper,
+  StyledButton,
+  StyledInput,
+  PStyled,
+  StyledDiv,
+} from '@/styles/StyledActivity';
 
 interface Props {
   userId: string;
@@ -14,32 +19,6 @@ interface Props {
   start: string;
   end: string;
 }
-
-const EventsWrapper = styled.div<WrapperProps>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  padding: 20px;
-  background-color: ${colors.background3};
-  color: white;
-`;
-
-const StyledButton = styled.button`
-  background-color: ${colors.primary};
-  border-radius: 20px;
-  color: black;
-  padding: 10px;
-  margin: 10px;
-  border: solid 1px white;
-  cursor: pointer;
-`;
-
-const StyledInput = styled.input`
-  padding: 10px;
-  margin: 10px;
-`;
 
 const postSchedule = async (data: Props) => {
   try {
@@ -79,19 +58,30 @@ const ScheduleForm = () => {
       <GlobalStyles />
       <EventsWrapper>
         <form style={{ display: 'flex', flexDirection: 'column' }}>
-          <label>Title:</label>
-          <StyledInput {...register('title', { required: 'Title is required' })} />
-          {errors.title && <p>{errors.title.message}</p>}
-          <label>Start:</label>
-          <StyledInput type='date' {...register('start', { required: 'Start date is required' })} />
-          {errors.start && <p>{errors.start.message}</p>}
-          <label>End:</label>
-          <StyledInput type='date' {...register('end', { required: 'End date is required' })} />
-          {errors.end && <p>{errors.end.message}</p>}
-          <label>Type:</label> <StyledInput {...register('type')} />
-          <label>Tag Line:</label> <StyledInput {...register('tagLine')} />
-          <label>Location:</label> <StyledInput {...register('location')} />
-          <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
+          <StyledDiv>
+            <PStyled style={{ color: '#D4D4D4', marginTop: '10%' }}>Schedule Info</PStyled>
+            <StyledInput
+              placeholder='Title'
+              {...register('title', { required: 'Title is required' })}
+            />
+            {errors.title && <p>{errors.title.message}</p>}
+            <StyledInput
+              placeholder='Start date'
+              type='date'
+              {...register('start', { required: 'Start date is required' })}
+            />
+            {errors.start && <p>{errors.start.message}</p>}
+            <StyledInput
+              type='date'
+              placeholder='End date'
+              {...register('end', { required: 'End date is required' })}
+            />
+            {errors.end && <p>{errors.end.message}</p>}
+            <StyledInput placeholder='Type' {...register('type')} />
+            <StyledInput placeholder='Tag Line' {...register('tagLine')} />
+            <StyledInput placeholder='Location' {...register('location')} />
+            <StyledButton onClick={handleSubmit(onSubmit)}>Submit</StyledButton>
+          </StyledDiv>
         </form>
       </EventsWrapper>
     </>
