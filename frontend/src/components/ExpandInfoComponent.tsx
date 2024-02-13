270fc5c6606a5905f6e4e098e80c21e42c8c9433
JohanBengtsson89 <johanbengtsson89@gmail.com>
2024-02-13 16:49:48 +0100

    Worked on the styling

--- frontend/src/components/ExpandInfoComponent.tsx
+++ frontend/src/components/ExpandInfoComponent.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import { StyledButton } from '@/styles/StyledActivity';
+import Drawer from '@mui/material/Drawer';
 import axios from 'axios';
+import { Article, Avatar, GlobalStyles, Location, Timeslot } from '@kokitotsos/react-components';
 
 interface ActivityType {
   id: number;
@@ -36,23 +36,32 @@ const ExpandInfo: React.FC<ExpandInfoProps> = ({ open, setOpen, activityId, chil
 
   return (
     <div>
+      <GlobalStyles />
       {/* <StyledButton onClick={() => setOpen(true)}>Open Drawer</StyledButton>; */}
-      <SwipeableDrawer
-        anchor='right'
-        open={open}
-        onClose={() => setOpen(false)}
-        onOpen={() => setOpen(true)}
-      >
-        <div style={{ width: '300px', color: 'white' }}>
-          <h1>Activity Details</h1>
-          <p>{response.title}</p>
+      <Drawer anchor='right' open={open} onClose={() => setOpen(false)}>
+        <div style={{ width: '400px', height: '700px', color: 'white', padding: '20px' }}>
+          <div style={{ display: 'flex' }}>
+            <Avatar
+              person={{
+                id: 'lorem',
+                name: 'Lorem Ipsum',
+                avatarSrc: 'https://picsum.photos/200',
+              }}
+              showFallback
+              width={80}
+            />
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <h2>Activity Details</h2>
+              <p>{response.title}</p>
+            </div>
+          </div>
           <p>Details about the activity</p>
           <p>{response.details}</p>
           <p>{response.start}</p>
           <p>{response.end}</p>
           {response.location && <p>{response.location.coordinates}</p>}
         </div>
-      </SwipeableDrawer>
+      </Drawer>
     </div>
   );
 };
