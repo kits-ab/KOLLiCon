d126c4fd0968af9a85e28f1d8b4d82c18d8caf33
AlirezaHK91 <113583919+AlirezaHK91@users.noreply.github.com>
2024-02-21 11:09:55 +0100

    Created a floating button component that remains when scrolling up and down. Button disappear when reach the bottom. Button becomes transparent when scrolling and becomes full color when not scrolling.

---
+++ frontend/src/components/Common/FloatingAddButton.tsx
@@ -0,0 +1,68 @@
+import Box from '@mui/material/Box';
+import { throttle } from 'lodash';
+import AddIcon from '@mui/icons-material/Add';
+import { useState, useEffect } from 'react';
+
+const FloatingButton = ({ activateDrawer }: { activateDrawer: () => void }) => {
+  const [showAddActivity, setShowAddActivity] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+    const handleScroll = throttle(() => {
+      clearTimeout(timeoutId);
+      setIsScrolling(true);
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const windowHeight = window.innerHeight;
+      const scrollPercentage = (scrollTop + windowHeight) / scrollHeight;
+      const triggerThreshold = 0.96;
+
+      setShowAddActivity(scrollPercentage >= triggerThreshold);
+
+      timeoutId = setTimeout(() => {
+        setIsScrolling(false);
+      }, 500);
+    }, 300);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <>
+      {!showAddActivity && (
+        <Box
+          sx={{
+            position: 'fixed',
+            bottom: '30px',
+            right: '20px',
+            zIndex: '1001',
+            opacity: isScrolling ? 0.4 : 1,
+            transition: 'opacity 0.5s ease-in-out',
+          }}
+        >
+          <AddIcon
+              sx={{
+                fontSize: '55px',
+                color: 'white',
+                borderRadius: '50%',
+                padding: '10px',
+                backgroundColor: '#596b4d',
+                cursor: 'pointer',
+                '&:hover': {
+                  backgroundColor: '#405632',
+                },
+              }}
+              onClick={activateDrawer}
+            />
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default FloatingButton;
