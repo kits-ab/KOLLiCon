6f5a06a5117b0baba0307dc5e5fa2afc1e3ef924
CodeMagnus1994 <magnusolsson1994@hotmail.se>
2024-02-22 14:31:32 +0100

    temporary finnished solution for export of md file.

--- frontend/src/pages/MenuHeader.tsx
+++ frontend/src/pages/MenuHeader.tsx
@@ -7,13 +7,12 @@ import { Link, useNavigate } from 'react-router-dom';
 import { signOut } from '@/utils/Auth';
 import MenuIcon from '@mui/icons-material/Menu';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { GlobalStyles, Text, width } from '@kokitotsos/react-components';
+import { GlobalStyles, Text } from '@kokitotsos/react-components';
 import Beer from '../assets/BearWithMe.png';
 import Box from '@mui/material/Box';
 import axios from 'axios';
 import { set } from 'react-hook-form';
 import { Schedule } from '@mui/icons-material';
-
 
 const drawerBleeding = 11;
 
@@ -22,7 +21,7 @@ interface Props {
 }
 
 interface Schedule {
-  id: string,
+  id: string;
 }
 
 const Root = styled('div')(() => ({
@@ -50,7 +49,6 @@ const MenuDiv = styled('div')(() => ({
   marginTop: '20px',
 }));
 
-
 const MenuItem = styled('p')(() => ({
   textAlign: 'center',
   fontSize: '1.1rem',
@@ -71,48 +69,35 @@ const FixedMenuIcon = styled(MenuIcon)(() => ({
 }));
 
 export default function SwipeableEdgeDrawer(props: Props) {
-
   const navigate = useNavigate();
 
   const { window } = props;
   const [open, setOpen] = React.useState(false);
   const [schedules, setSchedules] = React.useState<Schedule[]>([]);
+  const [selectedSchedule, setSelectedSchedule] = React.useState<Number | null>(null);
 
   const fetchAllSchedules = async () => {
     const responseOfSchedules = await axios.get('http://localhost:8080/api/allschedule');
     const theSchedules = responseOfSchedules.data;
     setSchedules(theSchedules);
-  }
+  };
 
-  const printScheduleToMarkdownFile = async (value : string | number) => {
-    console.log(value)
-    await axios.post(`http://localhost:8080/api/generateMdFile/1`);
-  }
-
-  const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log("yoyoy")
-    const selectedId = event.target.value;
-    const selectedSchedule = schedules.find((schedule) => schedule.id === selectedId);
-    if (selectedSchedule) {
-      printScheduleToMarkdownFile(selectedSchedule.id);
-    } else {
-      console.warn('No schedule selected.');
-    }
+  const printScheduleToMarkdownFile = async (value: number) => {
+    console.log('was also called wht value = ' + value);
+    await axios.post(`http://localhost:8080/api/generateMdFile/${value}`);
   };
 
   const menuItems = [
-    { label: 'Schema', link: '' },
-    { label: 'Min profil', link: '' },
-    { label: 'Tidigare KitsCons', link: '' },
-    { label: 
-      'Exportera Markdownfil',
-       link: '',
-       dropdownOptions: schedules.map((valueSchedule) => ({
+    { label: '', link: '' },
+    { label: '', link: '' },
+    { label: '', link: '' },
+    {
+      label: '',
+      dropdownOptions: schedules.map((valueSchedule) => ({
         label: valueSchedule.id,
         value: valueSchedule.id,
-       })),
-       onClick: () => handleDropdownChange,
-      },
+      })),
+    },
   ];
 
   const toggleDrawer = (newOpen: boolean) => () => {
@@ -124,7 +109,6 @@ export default function SwipeableEdgeDrawer(props: Props) {
   const logoutPage = () => {
     navigate('/login');
   };
-
 
   return (
     <>
@@ -162,32 +146,49 @@ export default function SwipeableEdgeDrawer(props: Props) {
           <MenuDiv>
             <Text>
               {menuItems.map((menuItem, index) => (
-                
                 <div key={index}>
-                <Link
-                  key={index}
-                  to={menuItem.link}
-                  style={{ textDecoration: 'none', color: 'white' }}
-                >
-                  <MenuItem
-                    style={{ textAlign: 'center', fontSize: '1.1rem', margin: '13px 0 13px 0' }}
+                  <Link
+                    key={index}
+                    to={menuItem.link}
+                    style={{ textDecoration: 'none', color: 'white' }}
                   >
-                    {menuItem.label}
-                  </MenuItem>
-                </Link>
-
-                 {menuItem.dropdownOptions && (
-                  <select onClick={fetchAllSchedules}>
-                    {menuItem.dropdownOptions.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </select>
-                )}
-              </div>
-
-            ))}
+                    <MenuItem
+                      style={{ textAlign: 'center', fontSize: '1.1rem', margin: '13px 0 13px 0' }}
+                    >
+                      {menuItem.label}
+                    </MenuItem>
+                  </Link>
+                  {menuItem.dropdownOptions && (
+                    <select
+                      style={{ width: '140px' }}
+                      onClick={fetchAllSchedules}
+                      onChange={(event) => setSelectedSchedule(Number(event.target.value))}
+                    >
+                      {menuItem.dropdownOptions.map((option) => (
+                        <option key={option.value} value={option.value}>
+                          {option.label}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+              ))}
+              <button
+                style={{
+                  backgroundColor: '#596b4d',
+                  padding: '6px',
+                  borderRadius: '6px',
+                  border: 'none',
+                  marginTop: '10%',
+                  marginLeft: '10%',
+                  color: '#d4d4d4',
+                  fontSize: '15px',
+                  marginBottom: '20px',
+                }}
+                onClick={() => printScheduleToMarkdownFile(selectedSchedule)}
+              >
+                Exportera md fil
+              </button>
               <Link to='https://beerwithme.se' style={{ textDecoration: 'none', color: 'white' }}>
                 <div
                   style={{
@@ -224,7 +225,8 @@ export default function SwipeableEdgeDrawer(props: Props) {
               }}
             />
             <Typography
-            component="div" style={{padding: '20px 0 20px 0'}}
+              component='div'
+              style={{ padding: '20px 0 20px 0' }}
               cursor='pointer'
               onClick={() => {
                 signOut();
