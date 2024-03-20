be2a0fabe2dada034a80b85f651df9677f0ba40b
AlirezaHK91 <113583919+AlirezaHK91@users.noreply.github.com>
2024-03-20 14:54:21 +0100

    Added and updated functions to update the presenter and extra presenter in activity service. changed also cascade type for presenters and extra.

--- backend/src/main/java/com/kollicon/service/ActivityService.java
+++ backend/src/main/java/com/kollicon/service/ActivityService.java
@@ -2,16 +2,14 @@ package com.kollicon.service;
 
 import com.kollicon.model.*;
 import com.kollicon.repository.ActivityRepository;
+import com.kollicon.repository.ExternalPresenterRepository;
 import com.kollicon.repository.PresenterRepository;
 import com.kollicon.repository.ScheduleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Consumer;
 
 @Service
@@ -22,6 +20,8 @@ public class ActivityService {
     PresenterRepository presenterRepository;
     @Autowired
     ScheduleRepository scheduleRepository;
+    @Autowired
+    ExternalPresenterRepository externalpresenterRepository;
 
 
     public ActivityModel createActivity(ActivityModel activity) {
@@ -62,6 +62,7 @@ public class ActivityService {
         updateFieldIfNotNull(existingActivity::setWinner, updatedActivity.getWinner());
 
         updatePresenters(existingActivity, updatedActivity.getPresenter());
+        updateExternalPresenters(existingActivity, updatedActivity.getExternalPresenter());
 
         updateLocation(existingActivity, updatedActivity.getLocation());
 
@@ -77,18 +78,58 @@ public class ActivityService {
     private void updatePresenters(ActivityModel existingActivity, List<PresenterModel> updatedPresenters) {
         if (updatedPresenters != null) {
             for (PresenterModel updatedPresenter : updatedPresenters) {
-                PresenterModel existingPresenter = presenterRepository.findById(updatedPresenter.getId())
-                        .orElseThrow(() -> new EntityNotFoundException("Presenter not found with id: " + updatedPresenter.getId()));
+                // If the presenter has an id of 0, it means it's a new presenter
+                if (updatedPresenter.getId() == 0) {
+                    // Set the activity for the new presenter
+                    updatedPresenter.setActivity(existingActivity);
+                    // Save the new presenter to generate a new ID
+                    presenterRepository.save(updatedPresenter);
+                } else {
+                    // If the presenter has a non-zero id, it's an existing presenter
+                    PresenterModel existingPresenter = presenterRepository.findById(updatedPresenter.getId())
+                            .orElseThrow(() -> new EntityNotFoundException("Presenter not found with id: " + updatedPresenter.getId()));
+                    // Update the fields of the existing presenter if new values are provided
+                    updateFieldIfNotNull(existingPresenter::setName, updatedPresenter.getName());
+                    updateFieldIfNotNull(existingPresenter::setAvatarSrc, updatedPresenter.getAvatarSrc());
 
-                updateFieldIfNotNull(existingPresenter::setName, updatedPresenter.getName());
-                updateFieldIfNotNull(existingPresenter::setAvatarSrc, updatedPresenter.getAvatarSrc());
+                    // Set the activity for the existing presenter
+                    existingPresenter.setActivity(existingActivity);
 
-                existingPresenter.setActivity(existingActivity);
-
-                presenterRepository.save(existingPresenter);
+                    // Save the existing presenter
+                    presenterRepository.save(existingPresenter);
+                }
             }
         }
     }
+
+    private void updateExternalPresenters(ActivityModel existingActivity, List<ExternalPresenterModel> updatedExtraPresenters) {
+        if (updatedExtraPresenters != null) {
+            for (ExternalPresenterModel updatedExtraPresenter : updatedExtraPresenters) {
+                // If the presenter has an id of 0, it means it's a new presenter
+                if (updatedExtraPresenter.getId() == 0) {
+                    // Set the activity for the new presenter
+                    updatedExtraPresenter.setActivity(existingActivity);
+                    // Save the new presenter to generate a new ID
+                    externalpresenterRepository.save(updatedExtraPresenter);
+                } else {
+                    // If the presenter has a non-zero id, it's an existing presenter
+                    ExternalPresenterModel existingPresenter = externalpresenterRepository.findById(updatedExtraPresenter.getId())
+                            .orElseThrow(() -> new EntityNotFoundException("External Presenter not found with id: " + updatedExtraPresenter.getId()));
+                    // Update the fields of the existing presenter if new values are provided
+                    updateFieldIfNotNull(existingPresenter::setName, updatedExtraPresenter.getName());
+                    updateFieldIfNotNull(existingPresenter::setAvatarSrc, updatedExtraPresenter.getAvatarSrc());
+
+                    // Set the activity for the existing presenter
+                    existingPresenter.setActivity(existingActivity);
+
+                    // Save the existing presenter
+                    externalpresenterRepository.save(existingPresenter);
+                }
+            }
+        }
+    }
+
+
 
     private void updateLocation(ActivityModel existingActivity, LocationModel updatedLocation) {
         if (updatedLocation != null) {
