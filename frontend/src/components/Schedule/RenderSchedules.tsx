f066dcd866cf025e03926c8db3b74d2e1bbda0d1
Christoffer Wallberg <112628645+ChristofferWallberg@users.noreply.github.com>
2024-03-26 16:18:06 +0100

    wip

--- frontend/src/components/Schedule/RenderSchedules.tsx
+++ frontend/src/components/Schedule/RenderSchedules.tsx
@@ -5,37 +5,43 @@ import { useState } from 'react';
 import { ExpandSchedule } from './ExpandSchedule';
 
 export const RenderSchedules = () => {
-  const [_, __, schedulesData] = useSchedule();
+  const [_, __, schedulesData, scheduleImages] = useSchedule();
   const [open, setOpen] = useState(false);
 
   const setScheduleOpen = () => {
     setOpen(!open);
   };
 
+  const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
   console.log('SchedulesData', schedulesData);
   return (
-    <div style={{ flexDirection: 'column', display: 'flex' }}>
+    <div style={{ flexDirection: 'column', display: 'flex', margin: '15px 10px 15px 10px' }}>
       {schedulesData.map((schedule: Schedule) => (
         <div key={schedule.id} style={{ flexDirection: 'row', display: 'flex' }}>
-          <div style={{ maxWidth: '100%', maxHeight: '100%', overflow: 'hidden' }}>
-            {schedule.imageUrl ? (
-              <img
-                src={schedule.imageUrl}
-                alt='Schedule Image'
-                style={{ maxWidth: '100%', maxHeight: '100%' }}
-              />
-            ) : (
-              <img
-                src='/placeholder.jpg'
-                alt='Placeholder Image'
-                style={{ maxWidth: '100%', maxHeight: '100%' }}
-              />
-            )}
-          </div>
-          <div>
+          {/* <div style={{ maxWidth: '100%', maxHeight: '100%', overflow: 'hidden' }}> */}
+          {schedule.imageUrl ? (
+            <img
+              src={schedule.imageUrl}
+              alt='Schedule Image'
+              style={{ maxWidth: '20%', maxHeight: '100%' }}
+            />
+          ) : (
+            <img
+              src={scheduleImages[0]}
+              alt='Placeholder Image'
+              style={{ maxWidth: '20%', maxHeight: '100%' }}
+            />
+          )}
+          {/* </div> */}
+          <div style={{ paddingLeft: '15px' }}>
             <div style={{ color: Colors.presentersGreen }}>{schedule.title}</div>
-            <div>{schedule.description}</div>
             <div>{schedule.location}</div>
+            <p>
+              {`${new Date(schedule.start).getDate()} - ${new Date(schedule.end).getDate()} ${capitalizeFirstLetter(new Intl.DateTimeFormat('sv-SE', { month: 'long' }).format(new Date(schedule.start)))}`}
+            </p>
           </div>
           <ExpandSchedule schedule={schedule} open={open} setOpen={setScheduleOpen} />
         </div>
