a121efa2602a2ba1ad7046095c9821b016fd36ba
JohanBengtsson89 <johanbengtsson89@gmail.com>
2024-04-09 16:11:14 +0200

    finihed for the day

--- frontend/src/utils/Hooks/useReview.tsx
+++ frontend/src/utils/Hooks/useReview.tsx
@@ -2,8 +2,13 @@ import { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Rating from '@mui/material/Rating';
 import axios from 'axios';
-import { TextAreaStyled, SubmitButton } from '@/styles/RegisterActivity/StyledActivity';
-import { da } from 'date-fns/locale';
+import {
+  TextAreaStyled,
+  SubmitButton,
+  ErrorStyled,
+} from '@/styles/RegisterActivity/StyledActivity';
+import styled from '@emotion/styled';
+import { Colors } from '@/styles/Common/colors';
 
 interface Props {
   userId: string;
@@ -13,52 +18,114 @@ interface Props {
 }
 
 const PostReviewComponent: React.FC<Props> = (props): React.ReactNode => {
-  const [value, setValue] = useState<number | null>(3);
+  const [rateValue, setRateValue] = useState<number | null>(null);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+  const [successSubmit, setSuccessSubmit] = useState<boolean>(false);
   const backendIP = import.meta.env.VITE_API_URL;
-
-  const postReview = async (data: Props) => {
-    try {
-      const response = await axios.post(`${backendIP}/api/review/create`, data);
-      console.log(response);
-      alert('Thanks for your feedback');
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const reviewForm = () => {
     const {
       register,
       handleSubmit,
       formState: { errors },
+      watch,
+      reset,
     } = useForm<Props>({
       mode: 'onChange',
     });
 
+    // Function to clear error after 10 seconds
+    const clearError = () => {
+      setTimeout(() => {
+        setSuccessSubmit(false);
+      }, 5000);
+    };
+
+    const review = watch('review');
+
+    const postReview = async (data: Props) => {
+      try {
+        const response = await axios.post(`${backendIP}/api/review/create`, data);
+        console.log(response);
+
+        reset({
+          userId: '',
+          activity: { id: 0 },
+          rate: 0,
+          review: '',
+        });
+        setRateValue(null);
+        setSuccessSubmit(true);
+        clearError();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     const onSubmit: SubmitHandler<Props> = (data) => {
-      data.rate = value || 2;
+      data.rate = rateValue || 0;
       data.userId = props.userId;
       data.activity = { id: props.activity };
-      console.log('data: ', data);
       postReview(data);
     };
 
+    useEffect(() => {
+      if (rateValue === null || review === '') {
+        setButtonDisabled(true);
+      } else {
+        setButtonDisabled(false);
+      }
+
+      // handleButton();
+    }, [rateValue, review]);
+
+    const StyledRating = styled(Rating)`
+      width: 60%;
+      max-width: 200px;
+      margin-bottom: 15px;
+      justify-content: space-between;
+      .MuiRating-iconEmpty {
+        color: gray;
+      }
+      .MuiRating-icon {
+        font-size: 30px;
+    `;
+
+    const SuccessMessage = styled(ErrorStyled)`
+      color: ${Colors.primaryAddButton};
+      display: flex;
+      justify-content: center;
+    `;
+
     return (
       <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Rating
-            name='simple-controlled'
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-          />
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <StyledRating
+              name='simple-controlled'
+              value={rateValue}
+              onChange={(event, newValue) => {
+                setRateValue(newValue);
+              }}
+            />
 
-          <TextAreaStyled placeholder='Ge feedbak' {...register('review', { required: true })} />
-          {errors.review && <span>This field is required</span>}
+            <TextAreaStyled
+              maxRows={3}
+              style={{ margin: '0px', width: '100%', height: '150px' }}
+              placeholder='Ge feedback'
+              {...register('review', { required: true })}
+            />
 
-          <SubmitButton type='submit'>Skicka</SubmitButton>
-        </form>
+            <SubmitButton
+              disabled={buttonDisabled}
+              sx={{ width: '150px', height: '30px' }}
+              type='submit'
+            >
+              Skicka
+            </SubmitButton>
+            {successSubmit && <SuccessMessage>Tack för din feedback</SuccessMessage>}
+          </form>
+        </div>
       </>
     );
   };
