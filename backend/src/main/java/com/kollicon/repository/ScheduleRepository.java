8ef4643264fe5e69e5c9ce11cedd3c97206b5faa
AlirezaHK91 <113583919+AlirezaHK91@users.noreply.github.com>
2024-04-12 10:02:35 +0200

    Completed the create activity service by adding active schedule dynamicly. Home page shows only the active schedule now.

--- backend/src/main/java/com/kollicon/repository/ScheduleRepository.java
+++ backend/src/main/java/com/kollicon/repository/ScheduleRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface ScheduleRepository extends JpaRepository<ScheduleModel, Long> {
     ScheduleModel findById(int i);
 
+    ScheduleModel active(boolean b);
 
 }
