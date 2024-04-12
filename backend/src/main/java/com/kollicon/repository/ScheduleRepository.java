7731f99ff2f204773721876d924d74f630f9d4c4
AlirezaHK91 <113583919+AlirezaHK91@users.noreply.github.com>
2024-04-12 10:54:28 +0200

    Set active status to false when creating new schedule.

--- backend/src/main/java/com/kollicon/repository/ScheduleRepository.java
+++ backend/src/main/java/com/kollicon/repository/ScheduleRepository.java
@@ -4,10 +4,14 @@ import com.kollicon.model.ScheduleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleModel, Long> {
     ScheduleModel findById(int i);
 
     ScheduleModel active(boolean b);
+
+    List <ScheduleModel> findAll();
 
 }
