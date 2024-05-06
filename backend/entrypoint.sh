bbfdc679bd216d838bcbc1a4b78ff1d6de7e4a3e
filols <filip.olsson@kits.se>
2024-05-06 20:03:57 +0200

    Remove outputs in entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Test outputs
-echo "DB_HOST: $DB_HOST"
-echo "DB_PORT: $DB_PORT"
-
 export SPRING_DATASOURCE_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}"
 export SPRING_DATASOURCE_USERNAME=${DB_USERNAME}
 export SPRING_DATASOURCE_PASSWORD=${DB_PASSWORD}
